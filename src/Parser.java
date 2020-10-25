df7968112cd747a9f75bbb06619e0ed1b7ce2abc
kamransagheir <kamran.sagheir@yahoo.com>
2020-10-25 00:42:12 -0400

    Refactored the Parser class and added documentation

--- src/Parser.java
+++ src/Parser.java
@@ -1,15 +1,24 @@
 import java.util.Scanner;
 
+/**
+ * The parser class is responsible for parsing the user entered commands
+ *
+ */
 public class Parser {
     private Scanner reader;
     private CommandWords commands;
-
+    /**
+     * Default constructor for the Parser class
+     */
     public Parser()
     {
         reader = new Scanner(System.in);
         commands = new CommandWords();
     }
-
+    /**
+     * This function gets the user entered command
+     * @return the user entered command
+     */
     public Command getCommand()
     {
         String input;
@@ -21,12 +30,18 @@ public class Parser {
         }
         return new Command(null);
     }
-
+    /**
+     * This function returns all the user entered commands
+     * @return all the user entered commands
+     */
     public String getCommands(){
         return commands.getCommandList();
     }
-
-    public String getIntroSelect(){
+    /**
+     * This function is responsible for getting the user entered input
+     * @return the user entered input
+     */
+    public String getUserInput(){
         String input;
 
         input = reader.nextLine();
