9a6e18c0bc42f891936e3e1c411f5d58e8355e10
kamransagheir <kamran.sagheir@yahoo.com>
2020-10-25 08:57:02 -0400

    Updated the country class and added java docs

--- src/CommandWords.java
+++ src/CommandWords.java
@@ -3,23 +3,30 @@ public class CommandWords {
     private static final String[] VALID_COMMANDS = {
       "help", "attack", "move", "map", "end turn"
     };
-
-    public boolean isCommand(String aString){
+    /**
+     * This function checks if the word is a command or not
+     * @param userInput the user entered value that needs to be checked if it is
+     * a command or not
+     * @return true if the user entered word is a command or not.
+     */
+    public boolean isCommand(String userInput){
         for(int i = 0; i < VALID_COMMANDS.length; i++){
-            if(VALID_COMMANDS[i].equals(aString)){
+            if(VALID_COMMANDS[i].equals(userInput)){
                 return true;
             }
         }
         return false;
     }
-
+    /**
+     * This function gets all the command word
+     * @return the list of all the commands
+     */
     public String getCommandList() {
-        // let's use a StringBuilder (not required)
         StringBuilder s = new StringBuilder();
         for(String command: VALID_COMMANDS) {
             s.append(command + "  ");
         }
         String str = s.toString();
-        return str.trim(); // removes spaces from beginning/end
+        return str.trim();
     }
 }
