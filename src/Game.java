1e815d61e9e85a0828d2bf84385d449b5dbb3064
duciaume <42221076+duciaume@users.noreply.github.com>
2020-10-25 17:23:14 -0400

    fixed issues

--- src/Game.java
+++ src/Game.java
@@ -340,7 +340,7 @@ public class Game {
         System.out.println(attackerC.getName() + " can attack:");
         for(int x = 0; x < neighbors.size(); x++){
             Country temp = neighbors.get(x);
-            if(temp.getPlayerOnCountry().getName() != player.getName()){
+            if(!temp.getPlayerOnCountry().getName().equals(player.getName())){
                 System.out.println(temp.getName() + " which has " + temp.getArmiesOnCountry() + " on it");
             }
             noOwnedNeighbors = false;
@@ -363,7 +363,7 @@ public class Game {
             }
             for (int y = 0; y < neighbors.size(); y++) {
                 Country temp = neighbors.get(y);
-                if(temp.getPlayerOnCountry().getName() != player.getName()){
+                if(!temp.getPlayerOnCountry().getName().equals(player.getName())){
                     if (temp.getName().equalsIgnoreCase(attacked.trim())) {
                         attackedC = temp;
                         attackedPlayer = temp.getPlayerOnCountry();
