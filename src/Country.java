ba02a2e371c9dff40e0f172dd613be27f9e3295c
kamransagheir <kamran.sagheir@yahoo.com>
2020-10-25 08:26:46 -0400

    Added javaDocs for the Country class and refactored the code

--- src/Country.java
+++ src/Country.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
-
+/**
+ * This class is responsible for dealing with the data
+ * belonging to the class, country.
+ *
+ */
 public class Country {
 
     private String name;
@@ -7,54 +11,88 @@ public class Country {
     private Player playerOnCountry;
     private int armiesOnCountry;
     private int occupied;
-
+    /**
+     * The constructor with one parameter
+     * @param name the name of the country
+     */
     public Country(String name){
         this.name = name;
         neighbors = new ArrayList<Country>();
         armiesOnCountry = 0;
         occupied = 0;
     }
-
-    public void addNeighbor(Country country){
-        neighbors.add(country);
+    /**
+     * This function adds the neighbors to the current country
+     * @param neighboringCountry 
+     */
+    public void addNeighbor(Country neighboringCountry){
+        neighbors.add(neighboringCountry);
     }
-
+    /**
+     * This function returns the neighbors
+     * @return the neighbors
+     */
     public ArrayList<Country> getNeighbors(){
         return neighbors;
     }
-
+    /**
+     * This function sets the country name
+     * @param name the name of the country
+     */
     public void setName(String name){
         this.name = name;
     }
-
+    /**
+     * This function gets the name of the country
+     * @return the name of the country
+     */
     public String getName(){
         return name;
     }
-
+    /**
+     * This person sets the player on the country
+     * @param playerOnCountry that player present on the country
+     */
     public void setPlayerOnCountry(Player playerOnCountry){
         this.playerOnCountry = playerOnCountry;
     }
-
+    /**
+     * Thus function gets the player that is located on the country 
+     * @return the player located on the country
+     */
     public Player getPlayerOnCountry(){
         return playerOnCountry;
     }
-
+    /**
+     * This function is responsible for adding armies to the country
+     * @param numOfArmy the number of armies to be added
+     */
     public void addArmiesOnCountry(int numOfArmy){
         armiesOnCountry = armiesOnCountry + numOfArmy;
     }
-
+    /**
+     * This funciton removes the armies from a country
+     * @param numOfArmy the number of armies to be removed from the country
+     */
     public void removeArmiesOnCountry(int numOfArmy){
         armiesOnCountry = armiesOnCountry - numOfArmy;
     }
-
+    /**
+     * This function gets the armies located on a country
+     * @return the armies located on the country
+     */
     public int getArmiesOnCountry(){
         return armiesOnCountry;
     }
-
+    /**
+     * This function sets a flag the country has been occupied
+     */
     public void setOccupied() {
         occupied = 1;
     }
-
+    /**
+     * This function checks if the country has been occupied or not
+     */
     public int getOccupied() {
         return occupied;
     }
