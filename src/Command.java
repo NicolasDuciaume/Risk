c7272599ef64018fe2dc3f9ecfda42807fea102b
kamransagheir <kamran.sagheir@yahoo.com>
2020-10-25 08:35:05 -0400

    Updated the command class and added the java docs

--- src/Command.java
+++ src/Command.java
@@ -1,15 +1,24 @@
 public class Command {
     
     private String commandWord;
-
+    /**
+     * The constructor of the class with one parameter
+     * @param commandWord the commandWord
+     */
     public Command(String commandWord){
         this.commandWord = commandWord;
     }
-
+    /**
+     * This function gets the command word
+     * @return the command word
+     */
     public String getCommandWord() {
         return commandWord;
     }
-
+    /**
+     * This function checks if the command is unknown or not
+     * @return true if the command is unknown or not
+     */
     public boolean isUnknown(){
         return (commandWord == null);
     }
