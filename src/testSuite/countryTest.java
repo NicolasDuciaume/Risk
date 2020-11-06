96628e7b72d3c3175ce6e4f7c22c9157b6f91337
kamransagheir <kamran.sagheir@yahoo.com>
2020-11-05 23:11:59 -0500

    Added test class for country

---
+++ src/testSuite/countryTest.java
@@ -0,0 +1,112 @@
+package testSuite;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+
+import model.Country;
+import model.Player;
+
+class countryTest {
+
+	String name;
+    ArrayList<Country> neighbors;
+    Player playerOnCountry;
+    int armiesOnCountry;
+    int occupied;
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	final void testCountry() {
+	}
+
+	@Test
+	final void testAddNeighbor() {
+		Country country = new Country("Canada");
+		Country neighbour = new Country("USA");
+		country.addNeighbor(neighbour);
+		ArrayList<Country> neighbours = new ArrayList<Country>();
+		neighbours.add(neighbour);
+		assertEquals(neighbours, country.getNeighbors());
+	}
+
+	@Test
+	final void testGetNeighbors() {
+		Country country = new Country("Canada");
+		Country neighbour = new Country("USA");
+		country.addNeighbor(neighbour);
+		ArrayList<Country> neighbours = new ArrayList<Country>();
+		neighbours.add(neighbour);
+		assertEquals(neighbours, country.getNeighbors());
+		
+
+	}
+	
+	@Test
+	final void testGetName() {
+		Country canada = new Country("Canda");
+		canada.setName("Canada");
+		assertEquals("Canada", canada.getName());
+	}
+	
+	@Test
+	final void testSetPlayerOnCountry() {
+		Player player = new Player("test");
+		Country canada = new Country("Canada");
+		canada.setPlayerOnCountry(player);
+		canada.getPlayerOnCountry();
+		assertEquals(player, canada.getPlayerOnCountry());
+	}
+
+	@Test
+	final void testGetPlayerOnCountry() {
+		Player player = new Player("test");
+		Country canada = new Country("Canada");
+		canada.setPlayerOnCountry(player);
+		canada.getPlayerOnCountry();
+		assertEquals(player, canada.getPlayerOnCountry());
+	}
+
+	@Test
+	final void testAddArmiesOnCountry() {
+		Country canada  = new Country("Test");
+		canada.addArmiesOnCountry(2);
+		assertEquals(2, canada.getArmiesOnCountry());
+	}
+
+	@Test
+	final void testRemoveArmiesOnCountry() {
+		Country canada  = new Country("Test");
+		canada.addArmiesOnCountry(2);
+		canada.removeArmiesOnCountry(1);
+		assertEquals(1, canada.getArmiesOnCountry());
+	}
+
+	@Test
+	final void testGetArmiesOnCountry() {
+		Country canada = new Country("Canda");
+		canada.addArmiesOnCountry(2);
+		assertEquals(2, canada.getArmiesOnCountry());
+	}
+
+
+	@Test
+	final void testGetOccupied() {
+		Country canada = new Country("Canda");
+		canada.setOccupied();
+		assertEquals(1, canada.getOccupied());
+	}
+
+}
