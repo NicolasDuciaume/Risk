639c94aff0e68834cd7fb89853e6badefe75baf3
kamransagheir <kamran.sagheir@yahoo.com>
2020-11-09 21:01:35 -0500

    Formatted and added java docs for the class

--- src/Risk.java
+++ src/Risk.java
@@ -1,16 +1,18 @@
-import Controller.GameController;
 import Controller.RiskController;
 import Model.*;
 import View.*;
 
 public class Risk {
+	/**
+	 * The main method for the game
+	 * 
+	 * @param args the arguments for the main method
+	 */
+	public static void main(String[] args) {
+		RiskModel model = new RiskModel();
+		RiskView view = new RiskView();
+		RiskController controller = new RiskController(model, view);
 
-    public static void main(String[] args) {
-        RiskModel model = new RiskModel();
-        RiskView view = new RiskView();
-        RiskController controller = new RiskController(model,view);
-
-    }
-
+	}
 
 }
