77ae61b27a8c9561a355ecc1dc8916de3d9b3b90
kamransagheir <kamran.sagheir@yahoo.com>
2020-11-09 20:53:33 -0500

    added docs for the game controller

--- src/Controller/GameController.java
+++ src/Controller/GameController.java
@@ -2,9 +2,7 @@ package Controller;
 
 import Model.*;
 import View.*;
-import Controller.*;
 
-import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -12,9 +10,11 @@ public class GameController implements ActionListener {
 
     private RiskModel model;
     private GameView view;
-
-    private SelectPlayerMenu selectPlayerMenu;
-
+    /**
+     * The constructor with two parameters for the GameController class
+     * @param model the moddel class
+     * @param view the view class
+     */
     public GameController(RiskModel model,GameView view){
         this.model = model;
         this.view = view;
@@ -22,7 +22,9 @@ public class GameController implements ActionListener {
         view.addActionListener(this);
     }
 
-
+    /**
+     * The action listener for the class
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getActionCommand().equals("EndTurn")){
