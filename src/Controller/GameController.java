1da7009ac9991a364d80754aa15cd7d2eb43a174
Chris Dsilva <chrisdsilva499@gmail.com>
2020-12-09 16:19:57 -0500

    Reformatted code

--- src/Controller/GameController.java
+++ src/Controller/GameController.java
@@ -26,29 +26,28 @@ public class GameController implements ActionListener {
     private MovementView movementView;
     private AttackView attack;
     private ArrayList<String> countries;
-    private static int saveNum = 1;
 
     /**
      * The constructor with two parameters for the GameController class
+     *
      * @param model the moddel class
-     * @param view the view class
+     * @param view  the view class
      */
-    public GameController(RiskModel model,GameView view){
+    public GameController(RiskModel model, GameView view) {
         this.model = model;
         this.view = view;
         this.countries = view.getCountries();
 
         //checks to see if the player has already reinforced in the case you load a save
-        if(!model.getCurrentPlayer().getReinforced()){
+        if (!model.getCurrentPlayer().getReinforced()) {
             reinforcements = new Reinforcements(this.view);
-            reinforcements.addActionListeners(new ReinforcementsController(reinforcements,model,view));
+            reinforcements.addActionListeners(new ReinforcementsController(reinforcements, model, view));
             reinforcements.setVisible(true);
         }
-
         view.addActionListener(this);
     }
 
-    public int getFileNumber(){
+    public int getFileNumber() {
         File f = new File(System.getProperty("user.dir"));
         FilenameFilter filterFiles = new FilenameFilter() {
             @Override
@@ -61,44 +60,38 @@ public class GameController implements ActionListener {
         return files.length + 1;
     }
 
-
-
     /**
      * The action listener for the class
      */
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getActionCommand().equals("EndTurn")){
+        if (e.getActionCommand().equals("EndTurn")) {
             movementView = new MovementView(this.view);
-            movementView.addActionListeners(new MovementController(movementView,model,view));
+            movementView.addActionListeners(new MovementController(movementView, model, view));
             movementView.setVisible(true);
 
-
-        }
-        else if(e.getActionCommand().equals("SaveGame")) {
+        } else if (e.getActionCommand().equals("SaveGame")) {
             //Model serialization
-            int decision = JOptionPane.showConfirmDialog(view,"Are you sure you would like to save this game?","Save Game",JOptionPane.YES_NO_OPTION);
-            if(decision == JOptionPane.YES_OPTION){
-                String filename = "Risk " + getFileNumber() +".txt";
+            int decision = JOptionPane.showConfirmDialog(view, "Are you sure you would like to save this game?", "Save Game", JOptionPane.YES_NO_OPTION);
+            if (decision == JOptionPane.YES_OPTION) {
+                String filename = "Risk " + getFileNumber() + ".txt";
                 model.save(filename);
-                JOptionPane.showMessageDialog(view,"File saved in working directory with file name " + filename);
-                int continueDialog = JOptionPane.showConfirmDialog(view,"Would you like to continue playing?","Continue",JOptionPane.YES_NO_OPTION);
-                if(continueDialog == JOptionPane.NO_OPTION){
+                JOptionPane.showMessageDialog(view, "File saved in working directory with file name " + filename);
+                int continueDialog = JOptionPane.showConfirmDialog(view, "Would you like to continue playing?", "Continue", JOptionPane.YES_NO_OPTION);
+                if (continueDialog == JOptionPane.NO_OPTION) {
                     view.dispose();
-                    new RiskController(new RiskModel(),new RiskView());
+                    new RiskController(new RiskModel(), new RiskView());
                 }
             }
-        }
-        else{
-            for(int i = 0; i < countries.size(); i++){
-                if(e.getActionCommand().equals(countries.get(i))){
-                    if(model.isOwned(countries.get(i))){
+        } else {
+            for (int i = 0; i < countries.size(); i++) {
+                if (e.getActionCommand().equals(countries.get(i))) {
+                    if (model.isOwned(countries.get(i))) {
                         attack = new AttackView(this.view);
-                        attack.addActionListeners(new AttackController(attack,model,view,countries.get(i)));
+                        attack.addActionListeners(new AttackController(attack, model, view, countries.get(i)));
                         attack.setVisible(true);
-                    }
-                    else{
-                        JOptionPane.showMessageDialog(view,"You do not own this country!");
+                    } else {
+                        JOptionPane.showMessageDialog(view, "You do not own this country!");
                     }
                 }
             }
